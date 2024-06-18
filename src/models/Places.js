05c815dd10c88346037ad0f8e8c72f05f67e415d
azhimn <mazhn34@gmail.com>
2024-06-18 20:21:07 +0800

    added get place details #28 (tested)

--- src/models/Places.js
+++ src/models/Places.js
@@ -1,25 +1,45 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import db from '../database/config.js';
+import Locations from './Locations.js';
 
 const Places = db.define('places', {
-    place_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+        place_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(55),
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING(8633)
+        },
+        category: {
+            type: DataTypes.STRING(18)
+        },
+        fk_location: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            references: {
+                model: 'locations',
+                key: 'location_id'                
+            }
+        },
+        coordinate: {
+            type: DataTypes.STRING(47)
+        },
     },
-    place_name: {
-        type: DataTypes.STRING(55),
-        allowNull: false,
-    },
-    description: {
+    {
+        tableName: 'places',
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+);
 
-    },
-    city: {
-
-    },
-    
-    tableName: 'places',
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'   
+Places.belongsTo(Locations, {
+    foreignKey: 'fk_location'
 });
+
+export default Places;
